48a39737b8b940fd64f335cafa7f5e41c8147171
AndreWebMsk <104007725+AndreWebMsk@users.noreply.github.com>
2022-05-05 15:46:01 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,22 @@
+$(function () {
 
-$(".how-to__form-input").click(function () {
+    $(".how-to__form-input").click(function () {
 
-    $header = $(this);
-    $content = $header.next();
-    $content.slideToggle(500, function () {
-        $header.text(function () {
+     $header = $(this);
+     $content = $header.next();
+     $content.slideToggle(500, function () {
+         $header.text(function () {
             return $content.is(":visible") ? "Collapse" : "Expand";
+          });
         });
     });
 
 
-$('.customers-slider').slick({
-    dots: true,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 2200
-  });
-});
+    $('.customers-slider').slick({
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2200
+        });
+    });
 
-if (typeof foo !== 'undefined') {
-    // Теперь мы знаем, что foo определено.
-  }
